3b1a0ef0d5f4e67c7a8517c851ee3cbad2e08c48
Mike Kostevich <m.kostevich@mageworx.com>
2021-11-01 20:25:21 +0300

    changed data type for properties 'min_value', 'predefined_values' (MwDonationsInfo), 'global_amount' and 'amount' (AddedMwDonation)

--- Model/Resolver/AddedDonation.php
+++ Model/Resolver/AddedDonation.php
@@ -13,7 +13,9 @@ use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
 use Magento\Framework\Serialize\SerializerInterface;
+use Magento\Store\Api\Data\StoreInterface;
 
 class AddedDonation implements ResolverInterface
 {
@@ -23,13 +25,20 @@ class AddedDonation implements ResolverInterface
     protected $serializer;
 
     /**
+     * @var PriceCurrencyInterface
+     */
+    protected $priceCurrency;
+
+    /**
      * AddedDonation constructor.
      *
      * @param SerializerInterface $serializer
+     * @param PriceCurrencyInterface $priceCurrency
      */
-    public function __construct(SerializerInterface $serializer)
+    public function __construct(SerializerInterface $serializer, PriceCurrencyInterface $priceCurrency)
     {
-        $this->serializer = $serializer;
+        $this->serializer    = $serializer;
+        $this->priceCurrency = $priceCurrency;
     }
 
     /**
@@ -59,18 +68,53 @@ class AddedDonation implements ResolverInterface
         if (empty($details)) {
             return null;
         }
-
-        $details      = $this->serializer->unserialize($details);
+        /** @var StoreInterface $store */
+        $store        = $context->getExtensionAttributes()->getStore();
+        $details      = (array)$this->serializer->unserialize($details);
         $isUseRoundUp = isset($details['isUseDonationRoundUp']) ? (bool)$details['isUseDonationRoundUp'] : null;
         $ukAddress    = isset($details['uk_address_for_gift_aid']) ? $details['uk_address_for_gift_aid'] : null;
 
         return [
             'charity_id'       => isset($details['charity_id']) ? (int)$details['charity_id'] : null,
             'charity_name'     => isset($details['charity_title']) ? $details['charity_title'] : null,
-            'global_amount'    => isset($details['global_donation']) ? (float)$details['global_donation'] : null,
-            'amount'           => isset($details['donation']) ? (float)$details['donation'] : null,
+            'global_amount'    => $this->getAmountData($details, 'global_donation', $store),
+            'amount'           => $this->getAmountData($details, 'donation', $store),
             'round_up'         => $isUseRoundUp,
             'gift_aid_address' => $ukAddress
+        ];
+    }
+
+    /**
+     * @param array $details
+     * @param string $key
+     * @param StoreInterface $store
+     * @return array|null
+     */
+    protected function getAmountData(array $details, string $key, StoreInterface $store): ?array
+    {
+        if (isset($details[$key])) {
+            return $this->getDonationAmountData((float)$details[$key], $store);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param float $value
+     * @param StoreInterface $store
+     * @return array
+     */
+    protected function getDonationAmountData(float $value, StoreInterface $store): array
+    {
+        return [
+            'value'         => $this->priceCurrency->roundPrice($value),
+            'currency_code' => $store->getCurrentCurrencyCode(),
+            'label'         => $this->priceCurrency->format(
+                $value,
+                false,
+                PriceCurrencyInterface::DEFAULT_PRECISION,
+                $store
+            )
         ];
     }
 }
--- Model/Resolver/DonationsInfo.php
+++ Model/Resolver/DonationsInfo.php
@@ -12,6 +12,8 @@ use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Store\Api\Data\StoreInterface;
 use MageWorx\Donations\Helper\Data as HelperData;
 use MageWorx\DonationsGraphQl\Model\CharitiesDataProvider;
 
@@ -28,15 +30,25 @@ class DonationsInfo implements ResolverInterface
     protected $charitiesDataProvider;
 
     /**
+     * @var PriceCurrencyInterface
+     */
+    protected $priceCurrency;
+
+    /**
      * DonationsInfo constructor.
      *
      * @param HelperData $helperData
      * @param CharitiesDataProvider $charitiesDataProvider
+     * @param PriceCurrencyInterface $priceCurrency
      */
-    public function __construct(HelperData $helperData, CharitiesDataProvider $charitiesDataProvider)
-    {
+    public function __construct(
+        HelperData $helperData,
+        CharitiesDataProvider $charitiesDataProvider,
+        PriceCurrencyInterface $priceCurrency
+    ) {
         $this->helperData            = $helperData;
         $this->charitiesDataProvider = $charitiesDataProvider;
+        $this->priceCurrency         = $priceCurrency;
     }
 
     /**
@@ -49,28 +61,59 @@ class DonationsInfo implements ResolverInterface
      */
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
-        $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+        /** @var StoreInterface $store */
+        $store = $context->getExtensionAttributes()->getStore();
 
         return [
-            'min_value'                         => $this->helperData->getMinimumDonation(),
+            'min_value'                         => $this->getDonationAmountData(
+                (float)$this->helperData->getMinimumDonation(),
+                $store
+            ),
             'default_description'               => $this->helperData->getDefaultDescription(),
             'amount_placeholder'                => $this->helperData->getAmountPlaceholder(),
             'default_charity_id'                => $this->helperData->getDefaultCharity(),
-            'predefined_values'                 => $this->getPredefinedValues(),
+            'predefined_values'                 => $this->getPredefinedValues($store),
             'is_donation_custom_amount_allowed' => $this->helperData->isShowDonationCustomAmount(),
             'allow_round_up'                    => $this->helperData->isShowRoundUpDonation(),
             'enable_round_up_by_default'        => $this->helperData->isRoundUpSelectedByDefault(),
             'is_gift_aid_allowed'               => $this->helperData->isGiftAidDonationsEnabled(),
             'gift_aid_message'                  => $this->helperData->getGiftAidDonationsMessage(),
-            'charities'                         => $this->charitiesDataProvider->getData($storeId)
+            'charities'                         => $this->charitiesDataProvider->getData((int)$store->getId())
         ];
     }
 
     /**
+     * @param float $value
+     * @param StoreInterface $store
      * @return array
      */
-    protected function getPredefinedValues(): array
+    protected function getDonationAmountData(float $value, StoreInterface $store): array
     {
-        return $this->helperData->isShowPredefinedValues() ? $this->helperData->getPredefinedValuesDonation() : [];
+        return [
+            'value'         => $this->priceCurrency->roundPrice($value),
+            'currency_code' => $store->getCurrentCurrencyCode(),
+            'label'         => $this->priceCurrency->format(
+                $value,
+                false,
+                PriceCurrencyInterface::DEFAULT_PRECISION,
+                $store
+            )
+        ];
+    }
+
+    /**
+     * @param StoreInterface $store
+     * @return array
+     */
+    protected function getPredefinedValues(StoreInterface $store): array
+    {
+        $result = [];
+        $values = $this->helperData->isShowPredefinedValues() ? $this->helperData->getPredefinedValuesDonation() : [];
+
+        foreach ((array)$values as $value) {
+            $result[] = $this->getDonationAmountData((float)$value, $store);
+        }
+
+        return $result;
     }
 }
